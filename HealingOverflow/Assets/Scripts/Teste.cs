cf222476d92dd600f5e21b1bd50e97eb7027b9eb
Gabriel Simmel Nascimento <galbrato@gmail.com>
2018-08-11 18:00:51 -0300

    Prefab do game manager criado e testado a mduança de cena do game manager, funcionando!

--- HealingOverflow/Assets/Scripts/Teste.cs
+++ HealingOverflow/Assets/Scripts/Teste.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Teste : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        gameObject.GetComponent<Rigidbody2D>().velocity = -transform.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("cartazScene");
+        }
 	}
 }
