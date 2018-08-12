fd23f2b54b3db34b5965136deb0e6dcc39a6d54f
guaxininja09 <ginja_gabriel@hotmail.com>
2018-08-12 00:31:50 -0300

    menus 12/08-00:31 deletei o textmeshpro

---
+++ HealingOverflow/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool isGamePaused = false;
+
+    private void Update()
+    {
+        
+    }
+
+    void Resume()
+    {
+        isGamePaused = false;
+    }
+}
